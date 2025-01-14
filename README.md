edf9eaca572c25686235fb5a498fcbe40d2b3dfa
JesusPM <jesus@perezdemiguel.com>
2025-01-14 20:11:51 +0100

    First commit from my own boilerplate

---
+++ README.md
@@ -0,0 +1,19 @@
+# Press & Win
+
+Chance based 2D mini game where the player is presented with 3 mystery gifts
+
+## Run
+
+To play the game in local environment just run
+
+```bash
+  npm run dev
+```
+
+## Build
+
+To create a new build of the game run
+
+```bash
+  npm run build
+```
---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import './global.css';
+import { Application, Container, Sprite } from 'pixi.js';
+import gsap from 'gsap';
+import PixiPlugin from 'gsap/PixiPlugin';
+import Environment from './systems/environment/environment';
+import { ENVIRONMENT_CONFIG } from './systems/environment/settings';
+import Game from './systems/game/game';
+
+(async () => {
+  const environment = new Environment(ENVIRONMENT_CONFIG);
+  const app = new Application();
+  const canvasContainerEl: HTMLElement | null =
+    document.querySelector('#canvas_container');
+
+  if (canvasContainerEl === null) {
+    throw new Error('Canvas container not found');
+  }
+
+  // Init PIXI
+  await app.init({
+    backgroundAlpha: 0,
+    resizeTo: canvasContainerEl,
+    resolution: environment.canvasResolution,
+    roundPixels: true,
+  });
+  canvasContainerEl.appendChild(app.canvas);
+
+  // Init GSAP
+  gsap.registerPlugin(PixiPlugin);
+  PixiPlugin.registerPIXI({ Container, Sprite });
+
+  // Init Game
+  await Game.init(app, environment);
+
+  window.Game = Game;
+})();
